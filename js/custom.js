384f50359a214580550a2aa44b2889ca788e405f
Hoose <hoose.muu@gmail.com>
2020-12-30 22:39:16 -0600

    ADD: Some improvements.

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
-let mymap = L.map('mapid').setView([19.2512285,-103.7406081], 13);
+/* let mymap = L.map('mapid').setView([19.2512285,-103.7406081], 13);
 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(mymap);
 let marker = L.marker([19.2512285,-103.7406081], {
   title: 'Lugar X'
-}).addTo(mymap);
+}).addTo(mymap); */
